454b44e80c013a900d84fbe63ae20c79ed21be9e
Chris Norman <17420369+chrnorm@users.noreply.github.com>
2022-06-17 13:05:57 +0100

    add test with other timezone

--- time_test.go
+++ time_test.go
@@ -9,12 +9,15 @@ import (
 func TestMarshalJSON(t *testing.T) {
 	type testcase struct {
 		name string
-		give time.Time
+		give Time
 		want string
 	}
 
+	loc := time.FixedZone("UTC+8", 8*3600)
+
 	testcases := []testcase{
-		{"ok", time.Date(2022, 01, 01, 10, 0, 0, 0, time.UTC), `"2022-01-01T10:00:00Z"`},
+		{"ok", New(time.Date(2022, 01, 01, 10, 0, 0, 0, time.UTC)), `"2022-01-01T10:00:00Z"`},
+		{"other timezone", New(time.Date(2022, 01, 01, 10, 0, 0, 0, loc)), `"2022-01-01T02:00:00Z"`}, // 10am in UTC+8 is 2am UTC
 	}
 
 	for _, tc := range testcases {
